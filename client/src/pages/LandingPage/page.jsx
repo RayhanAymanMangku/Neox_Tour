154c44afb815260a8e7c4ddd4fe91ecaa0e0460f
RayhanAymanMangku <22523093@students.uii.ac.id>
2024-06-26 18:10:17 +0700

    add: footer components

--- client/src/pages/LandingPage/page.jsx
+++ client/src/pages/LandingPage/page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { NavbarSticky } from "../../layouts/Navbar/NavbarComponent";
 import MainContentHome from "../../components/Content/Home";
+import { FooterComponents } from "../../layouts/Footer/FooterComponent";
 
 export const Home = () => {
   return (
     <div>
       <NavbarSticky />
       <MainContentHome />
+      <FooterComponents />
     </div>
   )
 };
