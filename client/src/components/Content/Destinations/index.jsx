be6289b1cc34dfb16b00b2b489aa8b9af78041af
RayhanAymanMangku <22523093@students.uii.ac.id>
2024-06-28 05:39:46 +0700

    fix: set content for destinations in index

---
+++ client/src/components/Content/Destinations/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroComponent from './Hero/HeroComponent';
+
+const MainContentDestinations = () => {
+    return (
+        <>
+            <HeroComponent />
+        </>
+    )
+}
+
+export default MainContentDestinations;
