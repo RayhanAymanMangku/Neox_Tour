52fc4e021af7d12047cee8a891b90c652c4ad7f8
RayhanAymanMangku <22523093@students.uii.ac.id>
2024-07-01 10:18:22 +0700

    edit app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
-import HomePage from "./pages/LandingPage/page";
-import DestinationPage from "./pages/DestinationPage/page";
+import { HomePage } from "./pages/LandingPage/page";
+import { DestinationPage } from "./pages/DestinationPage/page";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/Destinations" element={<DestinationPage />} />
+          <Route path="/" element={<Navigate to="/home" replace={true} />} />
         </Routes>
       </Router>
     </ThemeProvider>
